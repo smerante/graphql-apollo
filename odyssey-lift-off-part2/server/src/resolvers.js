2d5266f67701af8bb1b8300fec8df72a9b360b18
smerante <merante.s@hotmail.com>
2022-02-08 16:04:03 -0500

    initial setup for resolvers

---
+++ odyssey-lift-off-part2/server/src/resolvers.js
@@ -0,0 +1,12 @@
+
+const resolvers = {
+    Query: {
+        // Returns an array of Tracks that will be used to populate
+        // the homepage grid of our webapp
+        tracksFormHome: () => {
+            
+        }
+    }
+}
+
+module.exports = resolvers;
